fcecc4f5a2c8b2a6843310f68e67ef9560af579f
YoussefAbkar <102951010+YoussefAbkar@users.noreply.github.com>
2022-11-26 15:23:23 +0100

    première fonction

---
+++ Projet 1 Langue/Test Youssef.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+/*
+int main()
+{
+    int caract;
+    FILE* fic = NULL;
+    fic = fopen("Fichier1.txt","r");
+    if (fic != NULL)
+    {
+        do{caract = fgetc(fic);
+        printf("%c",caract);
+        } while (caract != EOF);
+            printf("\n");
+            fclose (fic);
+    } else
+        printf("Ouverture impossible");
+
+    return 0;
+}
+*/
--- Projet 1 Langue/main.cpp
+++ Projet 1 Langue/main.cpp
@@ -1,9 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
 #include <iostream>
+
 
 using namespace std;
 
+
+void Francais(){
+
+    int Tableau[25];
+
+
+    int caract;
+    FILE* fic = NULL;
+    fic = fopen("it.txt","r");
+    if (fic != NULL)
+    {
+        do{caract = fgetc(fic);
+        //printf("%c",caract);
+        cout << static_cast<char>(caract) <<endl;
+
+        } while (caract != EOF);
+            printf("\n");
+            fclose (fic);
+    } else
+        printf("Ouverture impossible");
+
+
+
+
+
+}
+/*
+à â ä ç é è ê ë î ï ô ö ù û ü ÿ
+À Â Ä Ç É È Ê Ë Î Ï Ô Ö Ù Û Ü Ÿ
+*/
+
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
+
+    Francais();
+
     return 0;
 }
+
